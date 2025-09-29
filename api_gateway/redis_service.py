183dde5f757b68db2874e4da7f54fa56296aabc1
swayam6901 <swayam837@gmail.com>
2025-09-29 18:46:34 +0530

    Delete api_gateway directory

--- api_gateway/redis_service.py
+++
@@ -1,8 +0,0 @@
-import os
-import redis
-
-redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-r = redis.from_url(redis_url)
-
-def enqueue_job(job_id, job_data):
-    r.rpush("summarizer_jobs", job_data)
